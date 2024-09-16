c909d4cdf5531d938dddfb25a55bf85985204039
david <xingkimb@gmail.com>
2024-09-16 12:27:43 -0400

    added player movement

--- code/main.py
+++ code/main.py
@@ -10,8 +10,10 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("space shooter")
 running = True
 
-x = 100
 player_surf = pygame.image.load(join("..","images","player.png")).convert_alpha()
+player_rect = player_surf.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
+
 star_surf = pygame.image.load(join("..","images","star.png")).convert_alpha()
 star_position = []
 
@@ -25,9 +27,12 @@ while running:
             running = False
 
     display_surface.fill("aquamarine")
-    display_surface.blit(player_surf, (x, 150))
+
     for x,y in star_position:
         display_surface.blit(star_surf,(x,y))
+    player_rect.left += 0.1
+    display_surface.blit(player_surf, player_rect)
+
     pygame.display.update()
 
 pygame.quit()
